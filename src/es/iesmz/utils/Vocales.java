1760f079828a12b32baa0cbe137cd043affb7047
Iris <irisaunav@gmail.com>
2026-05-27 22:43:41 +0200

    Borrar imports que no se están usando

--- src/es/iesmz/utils/Vocales.java
+++ src/es/iesmz/utils/Vocales.java
@@ -1,8 +1,5 @@
 package es.iesmz.utils;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Vocales {
     public static int contarVocales(String cadena) {
         int numVocales = 0;
