ad607957374137d2ff6b884025574d49cc5794f3
Iris <irisaunav@gmail.com>
2026-05-27 22:40:57 +0200

    Simplificar código con contains()

--- src/es/iesmz/utils/Vocales.java
+++ src/es/iesmz/utils/Vocales.java
@@ -4,17 +4,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Vocales {
-    private static final Set<Character> kVOCALES = new HashSet<>(Set.of('A', 'E', 'I', 'O', 'U'));
-
     public static int contarVocales(String cadena) {
         int numVocales = 0;
 
         for (int i = 0; i < cadena.length(); i++) {
-            for (Character vocal: kVOCALES) {
-                if (Character.toUpperCase(cadena.charAt(i)) == vocal) {
-                    numVocales++;
-                }
-            }
+            if ("AEIOUaeiou".contains(String.valueOf(cadena.charAt(i)))) numVocales++;
         }
 
         return numVocales;
